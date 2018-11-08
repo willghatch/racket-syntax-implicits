34bc2ddd7b646720e774a011ee93a70b5295169f
William G Hatch <william@hatch.uno>
2018-11-08 15:10:47 -0500

    add some comparisons with syntax parameters and normal identifiers

--- main.rkt
+++ main.rkt
@@ -101,6 +101,7 @@
 (module+ test
   (require
    rackunit
+   racket/stxparam
    (for-syntax
     racket/base
     syntax/parse
@@ -171,9 +172,9 @@
 
   (with-syntax-implicits ([chain-1
                            (syntax-parser
-                               [stx #`(list 'chain-1-c
-                                            #,(datum->syntax #'stx
-                                                             (list #'chain-2)))])])
+                             [stx #`(list 'chain-1-c
+                                          #,(datum->syntax #'stx
+                                                           (list #'chain-2)))])])
     (check-equal? (chain-1)
                   '(chain-1-c (chain-2-a (chain-3-a)))))
 
@@ -218,5 +219,142 @@
      (ignore-1)
      'in-macro-definitions-1))
 
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;; Compare let-syntax, syntax-parameterize, with-syntax-implicits
+  (define-syntax c-ls1 (syntax-parser [_ #''c-ls1]))
+  (define-syntax-parameter c-sp1 (syntax-parser [_ #''c-sp1]))
+  (define-syntax-implicit c-si1 (syntax-parser [_ #''c-si1]))
+
+  (splicing-let-syntax ([c-ls1 (syntax-parser [_ #''c-ls1_splice-1])])
+    (splicing-syntax-parameterize ([c-sp1 (syntax-parser [_ #''c-sp1_splice-1])])
+      (splicing-with-syntax-implicits
+       ([c-si1 (syntax-parser [_ #''c-si1_splice-1])])
+
+       (define-syntax (define-si-macro-that-uses-context stx)
+         (syntax-parse stx
+           [(_ name)
+            #'(define-syntax (name i-stx)
+                (syntax-parse i-stx
+                  [_ (datum->syntax #'name (list #'c-si1))]))]))
+       (define-syntax (define-si-macro-that-ignores-context stx)
+         (syntax-parse stx
+           [(_ name)
+            #'(define-syntax (name i-stx)
+                (syntax-parse i-stx
+                  [_ #'(c-si1)]))]))
+       (define-syntax (define-sp-macro-that-wants-to-use-context stx)
+         (syntax-parse stx
+           [(_ name)
+            #'(define-syntax (name i-stx)
+                (syntax-parse i-stx
+                  [_ (datum->syntax #'name (list #'c-sp1))]))]))
+       (define-syntax (define-sp-macro-that-ignores-context stx)
+         (syntax-parse stx
+           [(_ name)
+            #'(define-syntax (name i-stx)
+                (syntax-parse i-stx
+                  [_ #'(c-sp1)]))]))
+       (define-syntax (define-ls-macro-that-wants-to-use-context stx)
+         (syntax-parse stx
+           [(_ name)
+            #'(define-syntax (name i-stx)
+                (syntax-parse i-stx
+                  [_ (datum->syntax #'name (list #'c-ls1))]))]))
+       (define-syntax (define-ls-macro-that-ignores-context stx)
+         (syntax-parse stx
+           [(_ name)
+            #'(define-syntax (name i-stx)
+                (syntax-parse i-stx
+                  [_ #'(c-ls1)]))]))
+       (define-syntax (define-ls-macro-that-uses-context/hygiene-bending stx)
+         (syntax-parse stx
+           [(_ name)
+            #'(define-syntax (name i-stx)
+                (syntax-parse i-stx
+                  [_ (datum->syntax #'name (list 'c-ls1))]))]))
+       (define-syntax (define-ls-macro-that-uses-context/hygiene-bending-to-final-use stx)
+         (syntax-parse stx
+           [(_ name)
+            #'(define-syntax (name i-stx)
+                (syntax-parse i-stx
+                  [_ (datum->syntax i-stx (list 'c-ls1))]))]))
+       )))
+
+  (splicing-let-syntax ([c-ls1 (syntax-parser [_ #''c-ls1_splice-2])])
+    (splicing-syntax-parameterize ([c-sp1 (syntax-parser [_ #''c-sp1_splice-2])])
+      (splicing-with-syntax-implicits
+       ([c-si1 (syntax-parser [_ #''c-si1_splice-2])])
+       (define-si-macro-that-uses-context c-si1_use)
+       (define-si-macro-that-ignores-context c-si1_ignore)
+       (define-sp-macro-that-wants-to-use-context c-sp1_use)
+       (define-sp-macro-that-ignores-context c-sp1_ignore)
+       (define-ls-macro-that-wants-to-use-context c-ls1_use)
+       (define-ls-macro-that-ignores-context c-ls1_ignore)
+       (define-ls-macro-that-uses-context/hygiene-bending c-ls1_bend)
+       (define-ls-macro-that-uses-context/hygiene-bending-to-final-use c-ls1_bend-final)
+       )))
+
+
+  (splicing-let-syntax ([c-ls1 (syntax-parser [_ #''c-ls1_splice-3])])
+    (splicing-syntax-parameterize ([c-sp1 (syntax-parser [_ #''c-sp1_splice-3])])
+      (splicing-with-syntax-implicits
+       ([c-si1 (syntax-parser [_ #''c-si1_splice-3])])
+       #|
+       "use" here means that the defining macro bent the parenthesis but not the id.
+       Bending the id for syntax parameters and syntax implicits would mean likely
+       no longer being bound to the parameter/implicit anymore.
+       |#
+       ;;;; syntax parameters
+       (check-equal? c-sp1_ignore 'c-sp1_splice-3)
+       (check-equal? c-sp1_use 'c-sp1_splice-3)
+       (check-equal? c-sp1 'c-sp1_splice-3)
+       ;;;; syntax implicits
+       (check-equal? c-si1_ignore 'c-si1_splice-1)
+       (check-equal? c-si1_use 'c-si1_splice-2)
+       (check-equal? c-si1 'c-si1_splice-3)
+       ;;;; let-syntax (normal identifiers)
+       (check-equal? c-ls1_ignore 'c-ls1_splice-1)
+       (check-equal? c-ls1_use 'c-ls1_splice-1)
+       (check-equal? c-ls1 'c-ls1_splice-3)
+       (check-equal? c-ls1_bend 'c-ls1_splice-2)
+       (check-equal? c-ls1_bend-final 'c-ls1_splice-3)
+       )))
+
+  (let ()
+    ;; And now a context where things have not been let/parameterize/with-ed
+    ;;;; syntax parameters
+    (check-equal? c-sp1_ignore 'c-sp1)
+    (check-equal? c-sp1_use 'c-sp1)
+    (check-equal? c-sp1 'c-sp1)
+    ;;;; syntax implicits
+    (check-equal? c-si1_ignore 'c-si1_splice-1)
+    (check-equal? c-si1_use 'c-si1_splice-2)
+    (check-equal? c-si1 'c-si1)
+    ;;;; let-syntax (normal identifiers)
+    (check-equal? c-ls1_ignore 'c-ls1_splice-1)
+    (check-equal? c-ls1_use 'c-ls1_splice-1)
+    (check-equal? c-ls1 'c-ls1)
+    (check-equal? c-ls1_bend 'c-ls1_splice-2)
+    (check-equal? c-ls1_bend-final 'c-ls1)
+    )
+
+  (let-syntax ([c-sp1 (syntax-parser [_ #''not-a-syntax-parameter])]
+               [c-si1 (syntax-parser [_ #''not-a-syntax-implicit])])
+    ;; Syntax parameters and syntax implicits get to do their “bending” even
+    ;; if their names are shadowed.
+    (check-equal? c-sp1_ignore 'c-sp1)
+    (check-equal? c-sp1_use 'c-sp1)
+    (check-equal? c-si1_ignore 'c-si1_splice-1)
+    (check-equal? c-si1_use 'c-si1_splice-2)
+    )
+
+  #|
+  My “syntax implicits” are like syntax parameters in that they have a hygienic identifier while in another sense bending hygiene.
+  However, they can bend hygiene in a different way, more like normal identifiers.
+  They don't solve quite the same problem as syntax parameters (they are complimentary), but they have the hygienic identifier in the same way.
+  Are there other things in this space?  Syntax parameters get their value from the context where they end up, while syntax implicits get their value from some place where it was anchored (eg. by a source use OR a macro definition site).  Is there another place it would make sense to get a value from?
+
+  Also, what is a better name?  “Syntax Implicits” are so-named because I want to use them as *implicit* identifiers that may not even be bound in the context where they are used (IE they need syntax-parameter-style lookup that uses a hygienic name but a different context from the binding), but that get their binding from some anchor (more like a normal identifier that can be let-syntax-ed at a usage or at a macro definition site).  But their behavior in general has nothing to do with the implicit-ness of my use-case.
+  |#
 
   )
